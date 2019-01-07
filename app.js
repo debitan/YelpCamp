8bbe23e5f1eca6733c3b0a1a2f84af5293237869
David Matthews <dwm503@gmail.com>
2019-01-07 17:55:55 +0000

    add mLab database in place of c9 local database

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ var indexRoutes = require("./routes/index");
 // CONFIG
 // =================
 
-mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser : true});
+// mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser : true});
+mongoose.connect("mongodb://debitan:bCAasKvj2FkxA7G@ds251284.mlab.com:51284/yelpcamp", {useNewUrlParser: true});
 mongoose.set('useFindAndModify', false);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
